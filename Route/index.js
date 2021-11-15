268549d728671f9add84c174d1ec65e5674a58a5
Tejas281 <tejas1188245@gmail.com>
2021-11-15 13:08:22 +0530

    deployed : deployed on github pages

--- Route/index.js
+++ Route/index.js
@@ -1,24 +1,40 @@
 const router = require('express').Router();
 const user = require('../modle/user');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-router.post('/message', async(req, res) => {
-    const { username, email, contact, desc } = req.body;
+router.post('/message', async (req, res) => {
+  const { username, email, contact, desc } = req.body;
 
-    try {
-        const MessUser = await user.create({
-            username,
-            email,
-            contact,
-            desc
-        })
-        MessUser.save();
-        console.log('MSG', MessUser);
+  try {
+    const MessUser = await user.create({
+      username,
+      email,
+      contact,
+      desc,
+    });
+    MessUser.save();
+    console.log('MSG', MessUser);
 
-        res.status(200).json(MessUser);
-    } catch (error) {
-        res.status(400).json("Not working")
-    }
-})
+    res.status(200).json(MessUser);
+  } catch (error) {
+    res.status(400).json('Not working');
+  }
+});
+
+var data = {
+  username: 'dilip',
+  password: 'mypassword',
+};
+
+router.get('/messages/list', async (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  //   if (username === data.username && password === data.password) {
+  const allMessage = await user.find();
+  res.status(200).json({ data: allMessage });
+  //   }
+  //   return res.status(404).json({ message: 'Page not found' });
+});
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import './App.css';
 import { Main } from './Pages/index';
+import axios from 'axios';
+
+const Messages = (props) => {
+  React.useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/messages/list', {
+        username: props.match.params.username,
+        password: props.match.params.password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  });
+  return (
+    <div>
+      <h1>Messages</h1>
+      <p>USERNAME: {props.match.params.username}</p>
+      <p>PASSWORD: {props.match.params.password}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
       <Router>
         <Nav />
-        <Route exact path='/' component={Main} />
+        <Switch>
+          <Route
+            exact
+            path='/messages/:username/:password'
+            component={Messages}
+          />
+          <Route exact path='/' component={Main} />
+        </Switch>
       </Router>
     </>
   );
